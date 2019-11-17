9b2b6826ba36128959864fbe54081062083820bd
Ruslan <yarytsia@gmail.com>
2019-11-17 21:07:24 +0200

    disable s-curve change extruder fan speed

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -281,7 +281,7 @@
 #if ENABLED(USE_CONTROLLER_FAN)
   //#define CONTROLLER_FAN_PIN -1           // Set a custom pin for the controller fan
   #define CONTROLLERFAN_SECS 60             // Duration in seconds for the fan to run after all motors are disabled
-  #define CONTROLLERFAN_SPEED 200           // 255 == full speed //Ruslan
+  #define CONTROLLERFAN_SPEED 255           // 255 == full speed
   //#define CONTROLLERFAN_SPEED_Z_ONLY 127  // Reduce noise on machines that keep Z enabled
 #endif
 
@@ -357,7 +357,7 @@
 #define E5_AUTO_FAN_PIN -1
 #define CHAMBER_AUTO_FAN_PIN -1
 #define EXTRUDER_AUTO_FAN_TEMPERATURE 50
-#define EXTRUDER_AUTO_FAN_SPEED 200   // 255 == full speed //Ruslan
+#define EXTRUDER_AUTO_FAN_SPEED 180   // 255 == full speed //Ruslan
 #define CHAMBER_AUTO_FAN_TEMPERATURE 30
 #define CHAMBER_AUTO_FAN_SPEED 255
 
